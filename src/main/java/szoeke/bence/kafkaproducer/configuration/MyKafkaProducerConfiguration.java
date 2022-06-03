6c0ef9e363565e1e0b19d53e6032a0a421b5884e
Szőke Bence <bence.szoke@idomsoft.hu>
2022-06-03 22:31:25 +0200

    refact

--- src/main/java/szoeke/bence/kafkaproducer/configuration/MyKafkaProducerConfiguration.java
+++ src/main/java/szoeke/bence/kafkaproducer/configuration/MyKafkaProducerConfiguration.java
@@ -1,32 +1,46 @@
 package szoeke.bence.kafkaproducer.configuration;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.record.CompressionType;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import szoeke.bence.kafkaproducer.utility.EventSerializer;
 
 import java.util.Properties;
 
 public class MyKafkaProducerConfiguration {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyKafkaProducerConfiguration.class);
     private static final String BOOTSTRAP_SERVER_ENV_VAR = "BOOTSTRAP_SERVER";
     private static final String LINGER_MS_ENV_VAR = "LINGER_MS";
-    private static final String BATCH_SIZE_IN_KB_ENV_VAR = "BATCH_SIZE_IN_KB";
-    private static final String BUFFER_MEMORY_IN_KB_ENV_VAR = "BUFFER_MEMORY_IN_KB";
+    private static final String BATCH_SIZE_KB_ENV_VAR = "BATCH_SIZE_KB";
+    private static final String BUFFER_MEMORY_KB_ENV_VAR = "BUFFER_MEMORY_KB";
+    private static final String BUFFER_MEMORY = getSizeInBytes(BUFFER_MEMORY_KB_ENV_VAR);
+    private static final String LINGER_MS = System.getenv(LINGER_MS_ENV_VAR);
+    private static final String BATCH_SIZE = getSizeInBytes(BATCH_SIZE_KB_ENV_VAR);
 
     public Properties getProperties() {
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv(BOOTSTRAP_SERVER_ENV_VAR));
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, EventSerializer.class.getName());
-        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, System.getenv(LINGER_MS_ENV_VAR));
-        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, getSizeInBytes(BATCH_SIZE_IN_KB_ENV_VAR));
-        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, CompressionType.SNAPPY.name);
-        properties.setProperty(ProducerConfig.BUFFER_MEMORY_CONFIG, getSizeInBytes(BUFFER_MEMORY_IN_KB_ENV_VAR));
+        setPropertiesForBatchProducing(properties);
         return properties;
     }
 
-    private String getSizeInBytes(String envVarName) {
+    private void setPropertiesForBatchProducing(Properties properties) {
+        if (StringUtils.isNoneBlank(LINGER_MS, BATCH_SIZE, BUFFER_MEMORY)) {
+            properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, LINGER_MS);
+            properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, BATCH_SIZE);
+            properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, CompressionType.SNAPPY.name);
+            properties.setProperty(ProducerConfig.BUFFER_MEMORY_CONFIG, BUFFER_MEMORY);
+            LOGGER.info("Properties set for batch producing.");
+        }
+    }
+
+    private static String getSizeInBytes(String envVarName) {
         return String.valueOf(Long.parseLong(System.getenv(envVarName)) * 1024L);
     }
 }
--- src/main/java/szoeke/bence/kafkaproducer/producer/MyKafkaProducer.java
+++ src/main/java/szoeke/bence/kafkaproducer/producer/MyKafkaProducer.java
@@ -3,7 +3,7 @@ package szoeke.bence.kafkaproducer.producer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.VoidSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import szoeke.bence.kafkaproducer.configuration.MyKafkaProducerConfiguration;
@@ -11,23 +11,27 @@ import szoeke.bence.kafkaproducer.entity.Event;
 import szoeke.bence.kafkaproducer.utility.EventSerializer;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyKafkaProducer {
 
-    private static final String TOPIC_TO_SEND = "streams-input";
-    private static final long TEN_MINUTES_IN_NANOS = 600000000000L;
     private static final Logger LOGGER = LoggerFactory.getLogger(MyKafkaProducer.class);
-    private final KafkaProducer<String, Event> kafkaProducer;
-    private final List<Event> events;
+    private static final String TOPIC_TO_SEND = "streams-input";
+    private static final String DELAY_ENV_VAR = "DELAY_MS";
+    private static final long TEN_MINUTES_IN_NANOS = 600000000000L;
+    private static final long DELAY = Long.parseLong(System.getenv(DELAY_ENV_VAR));
+    private final KafkaProducer<Void, Event> kafkaProducer;
+    private final List<ProducerRecord<Void, Event>> producerRecords;
     private long sequenceNumber = 0L;
 
     public MyKafkaProducer(ObjectMapper objectMapper, MyKafkaProducerConfiguration configuration,
                            List<Event> events) {
         this.kafkaProducer = new KafkaProducer<>(
                 configuration.getProperties(),
-                new StringSerializer(),
+                new VoidSerializer(),
                 new EventSerializer(objectMapper));
-        this.events = events;
+        this.producerRecords = generateProducerRecords(events);
+        LOGGER.info(String.format("%d ms delay set between each event.\n", DELAY));
     }
 
     public void produce() {
@@ -36,24 +40,46 @@ public class MyKafkaProducer {
             LOGGER.info(String.format("The producing has stopped after 10 minutes and %d events.", sequenceNumber));
         } catch (Exception e) {
             LOGGER.error(String.format("Exception occurred while producing messages: %s", e.getMessage()));
+        } finally {
+            kafkaProducer.flush();
         }
     }
 
     private void sendEventsToTopic() {
         long endTime = System.nanoTime() + TEN_MINUTES_IN_NANOS;
         while (System.nanoTime() < endTime) {
-            events
+            producerRecords
                     .parallelStream()
                     .forEach(this::sendSingleEventToTopic);
         }
     }
 
-    private void sendSingleEventToTopic(Event event) {
-        ProducerRecord<String, Event> record = new ProducerRecord<>(
+    private void sendSingleEventToTopic(ProducerRecord<Void, Event> producerRecord) {
+        sleepIfNeeded();
+        kafkaProducer.send(producerRecord);
+        sequenceNumber++;
+    }
+
+    private List<ProducerRecord<Void, Event>> generateProducerRecords(List<Event> events) {
+        return events
+                .stream()
+                .map(this::generateSingleProducerRecord)
+                .collect(Collectors.toList());
+    }
+
+    private ProducerRecord<Void, Event> generateSingleProducerRecord(Event event) {
+        return new ProducerRecord<>(
                 TOPIC_TO_SEND,
-                String.valueOf(++sequenceNumber),
                 event);
-        kafkaProducer.send(record);
-        kafkaProducer.flush();
+    }
+
+    private void sleepIfNeeded() {
+        if (DELAY > 0) {
+            try {
+                Thread.sleep(DELAY);
+            } catch (InterruptedException e) {
+                LOGGER.error("Exception occurred by sleep for delay: ", e);
+            }
+        }
     }
 }
