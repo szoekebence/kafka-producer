db2d82a096c7ca68b64b3f3a0472e0ff8c17f4b0
Szőke Bence <bence.szoke@idomsoft.hu>
2022-06-19 14:10:05 +0200

    add key

--- src/main/java/szoeke/bence/kafkaproducer/producer/MyKafkaProducer.java
+++ src/main/java/szoeke/bence/kafkaproducer/producer/MyKafkaProducer.java
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.VoidSerializer;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import szoeke.bence.kafkaproducer.configuration.MyKafkaProducerConfiguration;
 import szoeke.bence.kafkaproducer.utility.JsonNodeSerializer;
 
-import java.time.Duration;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MyKafkaProducer {
 
@@ -21,17 +19,17 @@ public class MyKafkaProducer {
     private static final String DELAY_ENV_VAR = "DELAY_MS";
     private static final long TEN_MINUTES_IN_NANOS = 600000000000L;
     private static final long DELAY = Long.parseLong(System.getenv(DELAY_ENV_VAR));
-    private final KafkaProducer<Void, JsonNode> kafkaProducer;
-    private final List<ProducerRecord<Void, JsonNode>> producerRecords;
+    private final KafkaProducer<Long, JsonNode> kafkaProducer;
+    private final List<JsonNode> events;
     private long sequenceNumber = 0L;
 
     public MyKafkaProducer(ObjectMapper objectMapper, MyKafkaProducerConfiguration configuration,
                            List<JsonNode> events) {
         this.kafkaProducer = new KafkaProducer<>(
                 configuration.getProperties(),
-                new VoidSerializer(),
+                new LongSerializer(),
                 new JsonNodeSerializer(objectMapper));
-        this.producerRecords = generateProducerRecords(events);
+        this.events = events;
         LOGGER.info(String.format("%d ms delay set between each event.\n", DELAY));
     }
 
@@ -42,7 +40,7 @@ public class MyKafkaProducer {
             LOGGER.error(String.format("Exception occurred while producing messages: %s", e.getMessage()));
         } finally {
             kafkaProducer.flush();
-            kafkaProducer.close(Duration.ofSeconds(10L));
+            kafkaProducer.close();
             LOGGER.info(String.format("The producing has stopped after 10 minutes and %d events.", sequenceNumber));
             sleepIfNeeded(10000);
         }
@@ -51,28 +49,22 @@ public class MyKafkaProducer {
     private void sendEventsToTopic() {
         long endTime = System.nanoTime() + TEN_MINUTES_IN_NANOS;
         while (System.nanoTime() < endTime) {
-            producerRecords
+            events
                     .parallelStream()
                     .forEach(this::sendSingleEventToTopic);
         }
     }
 
-    private void sendSingleEventToTopic(ProducerRecord<Void, JsonNode> producerRecord) {
+    private void sendSingleEventToTopic(JsonNode event) {
         sleepIfNeeded(DELAY);
-        kafkaProducer.send(producerRecord);
+        kafkaProducer.send(generateSingleProducerRecord(event));
         sequenceNumber++;
     }
 
-    private List<ProducerRecord<Void, JsonNode>> generateProducerRecords(List<JsonNode> events) {
-        return events
-                .stream()
-                .map(this::generateSingleProducerRecord)
-                .collect(Collectors.toList());
-    }
-
-    private ProducerRecord<Void, JsonNode> generateSingleProducerRecord(JsonNode event) {
+    private ProducerRecord<Long, JsonNode> generateSingleProducerRecord(JsonNode event) {
         return new ProducerRecord<>(
                 TOPIC_TO_SEND,
+                System.nanoTime(),
                 event);
     }
 
