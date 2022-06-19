dd9f8b1a1458a7322ca0a44393951caacd2d9391
Szőke Bence <bence.szoke@idomsoft.hu>
2022-06-19 14:40:48 +0200

    add key seqNum

--- src/main/java/szoeke/bence/kafkaproducer/producer/MyKafkaProducer.java
+++ src/main/java/szoeke/bence/kafkaproducer/producer/MyKafkaProducer.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import szoeke.bence.kafkaproducer.configuration.MyKafkaProducerConfiguration;
@@ -19,7 +19,7 @@ public class MyKafkaProducer {
     private static final String DELAY_ENV_VAR = "DELAY_MS";
     private static final long TEN_MINUTES_IN_NANOS = 600000000000L;
     private static final long DELAY = Long.parseLong(System.getenv(DELAY_ENV_VAR));
-    private final KafkaProducer<Long, JsonNode> kafkaProducer;
+    private final KafkaProducer<String, JsonNode> kafkaProducer;
     private final List<JsonNode> events;
     private long sequenceNumber = 0L;
 
@@ -27,7 +27,7 @@ public class MyKafkaProducer {
                            List<JsonNode> events) {
         this.kafkaProducer = new KafkaProducer<>(
                 configuration.getProperties(),
-                new LongSerializer(),
+                new StringSerializer(),
                 new JsonNodeSerializer(objectMapper));
         this.events = events;
         LOGGER.info(String.format("%d ms delay set between each event.\n", DELAY));
@@ -58,13 +58,12 @@ public class MyKafkaProducer {
     private void sendSingleEventToTopic(JsonNode event) {
         sleepIfNeeded(DELAY);
         kafkaProducer.send(generateSingleProducerRecord(event));
-        sequenceNumber++;
     }
 
-    private ProducerRecord<Long, JsonNode> generateSingleProducerRecord(JsonNode event) {
+    private ProducerRecord<String, JsonNode> generateSingleProducerRecord(JsonNode event) {
         return new ProducerRecord<>(
                 TOPIC_TO_SEND,
-                System.nanoTime(),
+                String.valueOf(sequenceNumber++),
                 event);
     }
 
