18109d62d6b5816dfb6bdee66e1488acc853eaef
Szőke Bence <bence.szoke@idomsoft.hu>
2022-09-10 20:36:08 +0200

    remove key generation and delay setting

--- src/main/java/szoeke/bence/kafkaproducer/producer/MyKafkaProducer.java
+++ src/main/java/szoeke/bence/kafkaproducer/producer/MyKafkaProducer.java
@@ -16,13 +16,10 @@ public class MyKafkaProducer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MyKafkaProducer.class);
     private static final String TOPIC_TO_SEND = "streams-input";
-    private static final String DELAY_ENV_VAR = "DELAY_MS";
     private static final String PRODUCING_TIME_MIN_ENV_VAR = "PRODUCING_TIME_MIN";
     private static final long PRODUCING_TIME_NANOS = Long.parseLong(System.getenv(PRODUCING_TIME_MIN_ENV_VAR)) * 60000000000L;
-    private static final long DELAY = Long.parseLong(System.getenv(DELAY_ENV_VAR));
     private final KafkaProducer<String, JsonNode> kafkaProducer;
     private final List<JsonNode> events;
-    private long sequenceNumber = 0L;
 
     public MyKafkaProducer(ObjectMapper objectMapper, MyKafkaProducerConfiguration configuration,
                            List<JsonNode> events) {
@@ -31,49 +28,34 @@ public class MyKafkaProducer {
                 new StringSerializer(),
                 new JsonNodeSerializer(objectMapper));
         this.events = events;
-        LOGGER.info("{} ms delay set between each event.\n", DELAY);
     }
 
     public void produce() {
         try (kafkaProducer) {
-            sendEventsToTopic();
+            sendEvents();
         } catch (Exception e) {
             LOGGER.error("Exception occurred while producing messages: {}", e.getMessage());
         } finally {
-            LOGGER.info("The producing has stopped after {} minutes and {} events.",
-                    System.getenv(PRODUCING_TIME_MIN_ENV_VAR), sequenceNumber);
-            sleepIfNeeded(10000);
+            LOGGER.info("The producing has stopped after {} minutes.", System.getenv(PRODUCING_TIME_MIN_ENV_VAR));
         }
     }
 
-    private void sendEventsToTopic() {
+    private void sendEvents() {
         long endTime = System.nanoTime() + PRODUCING_TIME_NANOS;
         while (System.nanoTime() < endTime) {
             events
                     .parallelStream()
-                    .forEach(this::sendSingleEventToTopic);
+                    .forEach(this::sendSingleEvent);
         }
     }
 
-    private void sendSingleEventToTopic(JsonNode event) {
-        sleepIfNeeded(DELAY);
-        kafkaProducer.send(generateSingleProducerRecord(event));
+    private void sendSingleEvent(JsonNode event) {
+        kafkaProducer.send(generateProducerRecord(event));
     }
 
-    private ProducerRecord<String, JsonNode> generateSingleProducerRecord(JsonNode event) {
+    private ProducerRecord<String, JsonNode> generateProducerRecord(JsonNode event) {
         return new ProducerRecord<>(
                 TOPIC_TO_SEND,
-                String.valueOf(sequenceNumber++),
                 event);
-    }
-
-    private void sleepIfNeeded(long delay) {
-        if (delay > 0) {
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                LOGGER.error("Exception occurred by sleep for delay: ", e);
-            }
-        }
     }
 }
